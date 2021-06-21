32045bad67dd0c7cfe180d349888f2609925a3b1
Antoine de Prieelle <antoine_@live.nl>
2021-06-21 18:10:08 +0200

    Add touch support for react component SelectTimeOverlayRect

--- src/components/aWeatherRainGraphScaleLogFolder/SelectTimeOverlayRect.js
+++ src/components/aWeatherRainGraphScaleLogFolder/SelectTimeOverlayRect.js
@@ -1,6 +1,20 @@
 import * as d3 from 'd3';
 
-export const SelectTimeOverlayRect = ({ innerWidth, innerHeight, setXCoord }) => {
+export const SelectTimeOverlayRect = ({ 
+  innerWidth, 
+  innerHeight, 
+  setXCoord
+}) => {
+  const calculateXCoordValue = (event) => {
+    if ((d3.pointer(event.touches[0], event.target)[0]) < 0) {
+      return 0;
+    } else if ((d3.pointer(event.touches[0], event.target)[0]) > innerWidth) {
+      return innerWidth;
+    } else {
+      return (d3.pointer(event.touches[0], event.target)[0]);
+    }
+  }
+
   return (
     <rect
       className="select-time-overlay-rect"
@@ -13,6 +27,21 @@ export const SelectTimeOverlayRect = ({ innerWidth, innerHeight, setXCoord }) =>
       // onMouseLeave={() => {
       //   setXCoord(null);
       // }}
+
+
+      // d3.pointer target default to currentTarget which is null with onTouchMove,
+      // "target" is the one that needs to be set as the target in d3.pointer
+      onTouchMove={(event) => {
+        // console.log(d3.pointer(event.touches[0], event.target)[0]);
+        console.log(d3.pointer(event.touches[0], event.target)[0]);
+        const xCoordValue = calculateXCoordValue(event);
+        console.log(xCoordValue);
+        setXCoord(xCoordValue);
+        // setXCoord(d3.pointer(event.touches[0], event.target)[0]);
+        // setXCoord(d3.pointer(event.touches[0], event.target)[0]);
+        // setXCoord(d3.pointer(event.touches[0], event.target)[0]);
+        // console.log(event.touches[0]);
+      }}
     />
   )
 }
